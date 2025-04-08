8692b83fba2eab415543f387495ec3d23c4b6fcd
devyej8753 <devyej8753@gmail.com>
2025-04-08 15:12:21 +0900

    [lyj] 채팅 기능 작성중
    	- 래파지토리 생성
    	- 서비스 생성
    	- dto 생성

--- eroom/src/main/java/com/eroom/chat/controller/ChatController.java
+++ eroom/src/main/java/com/eroom/chat/controller/ChatController.java
@@ -1,9 +1,14 @@
 package com.eroom.chat.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.eroom.chat.entity.Chatroom;
+import com.eroom.chat.service.ChatroomService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,9 +17,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChatController {
 
+	private final ChatroomService chatroomService;
+	
 	@GetMapping("/list")
 	public String selectChatRoomAll(Model model) {
-		
+		List<Chatroom> resultList = chatroomService.selectChatRoomAll;
 		return "chat/list";
 	}
 }
---
+++ eroom/src/main/java/com/eroom/chat/service/ChatroomService.java
@@ -0,0 +1,14 @@
+package com.eroom.chat.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.eroom.chat.entity.Chatroom;
+
+@Service
+public class ChatroomService {
+
+	public List<Chatroom> selectChatRoomAll;
+
+}
