080f22dde6e1514c74021038e1e90aaa1fbb52f9
KYG17 <sjkyg17@gmail.com>
2025-04-08 15:11:43 +0900

    [KYK] 수정중

--- eroom/src/main/java/com/eroom/calendar/controller/CalendarController.java
+++ eroom/src/main/java/com/eroom/calendar/controller/CalendarController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CalendarController {
 	
 	//캘린더 개인일정 목록으로 화면 전환
-	@GetMapping("/calendar")
+	@GetMapping("/calendar/employee")
 	public String employeeCalendarView() {
-		return "calendar/list";
+		return "calendar/employeelist";
 	}
 
 }
--- eroom/src/main/java/com/eroom/directory/entity/Employee.java
+++ eroom/src/main/java/com/eroom/directory/entity/Employee.java
@@ -1,57 +1,57 @@
-package com.eroom.directory.entity;
-
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
-@Builder
-
-@Entity
-@Table(name="employee")
-public class Employee {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long employeeNo; // 사번
-	
-	@Column(name="employee_pw")
-	private String employeePw; // 비밀번호
-	@Column(name="employee_name")
-	private String employeeName; // 이름
-	@Column(name="employee_hire_date")
-	private LocalDateTime employeeHireDate; // 고용일
-	@Column(name="employee_end_date")
-	private LocalDateTime employeeEndDate; // 퇴사일
-	@Column(name="employee_end_yn")
-	private String employeeEndYn; // 퇴사여부
-	@Column(name="employee_position")
-	private String employeePosition; // 직급	
-	
-	@ManyToOne
-	@JoinColumn(name="department_no")
-	private Department department; // 부서번호
-	@ManyToOne
-	@JoinColumn(name="team_no")
-	private Team team; // 팀번호
-	
-	
-	
-}
+//package com.eroom.directory.entity;
+//
+//import java.time.LocalDateTime;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.ManyToOne;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//@Builder
+//
+//@Entity
+//@Table(name="employee")
+//public class Employee {
+//	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long employeeNo; // 사번
+//	
+//	@Column(name="employee_pw")
+//	private String employeePw; // 비밀번호
+//	@Column(name="employee_name")
+//	private String employeeName; // 이름
+//	@Column(name="employee_hire_date")
+//	private LocalDateTime employeeHireDate; // 고용일
+//	@Column(name="employee_end_date")
+//	private LocalDateTime employeeEndDate; // 퇴사일
+//	@Column(name="employee_end_yn")
+//	private String employeeEndYn; // 퇴사여부
+//	@Column(name="employee_position")
+//	private String employeePosition; // 직급	
+//	
+//	@ManyToOne
+//	@JoinColumn(name="department_no")
+//	private Department department; // 부서번호
+//	@ManyToOne
+//	@JoinColumn(name="team_no")
+//	private Team team; // 팀번호
+//	
+//	
+//	
+//}
