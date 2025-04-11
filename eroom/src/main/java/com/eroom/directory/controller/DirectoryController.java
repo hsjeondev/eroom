b4fc8c37ff9de2c43889033c05bf030763ca8aa7
devkskg <devkskg@gmail.com>
2025-04-11 17:15:40 +0900

    [ksk] 주소록 트리구조 css 완료 결재 css 작성중3_20250411
    	- 주소록 트리 구조 css 완료
    	- 결재 등록 css 작성중

--- eroom/src/main/java/com/eroom/directory/controller/DirectoryController.java
+++ eroom/src/main/java/com/eroom/directory/controller/DirectoryController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.eroom.directory.dto.EmployeeDirectoryDto;
 import com.eroom.directory.entity.EmployeeDirectory;
