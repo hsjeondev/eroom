30e7d3ab12b3136edbeab0f85ceffea90ba10e36
KYG17 <sjkyg17@gmail.com>
2025-04-15 19:39:22 +0900

    [KYK] 수정 editor까지 들어가는거 확인.

--- eroom/src/main/java/com/eroom/calendar/dto/EmployeeCalendarDto.java
+++ eroom/src/main/java/com/eroom/calendar/dto/EmployeeCalendarDto.java
@@ -27,6 +27,7 @@ public class EmployeeCalendarDto {
 	private LocalDateTime calendar_end_time; // 종료 시간
 	private String calendar_content; // 설명
 	private String calendar_creator;
+	private String calendar_editor;
 	private Long employee_no; // 사번
 	private String separator; // 구분자
 	private String visibleYn = "Y";
@@ -37,7 +38,7 @@ public class EmployeeCalendarDto {
 		return EmployeeCalendar.builder().calendarNo(calendar_no).calendarTitle(calendar_title)
 				.calendarLocation(calendar_location).calendarStartTime(calendar_start_time)
 				.calendarEndTime(calendar_end_time).calendarContent(calendar_content).calendarCreator(calendar_creator)
-				.employeeNo(employee_no).separator(separator).visibleYn(visibleYn)
+				.employeeNo(employee_no).separator(separator).visibleYn(visibleYn).calendarEditor(calendar_editor)
 
 				.build();
 	}
@@ -50,6 +51,7 @@ public class EmployeeCalendarDto {
 				.calendar_end_time(employeeCalendar.getCalendarEndTime())
 				.calendar_content(employeeCalendar.getCalendarContent())
 				.calendar_creator(employeeCalendar.getCalendarCreator())
+				.calendar_editor(employeeCalendar.getCalendarEditor())
 				.employee_no(employeeCalendar.getEmployeeNo())
 				.separator(employeeCalendar.getSeparator()).calendar_reg_date(employeeCalendar.getCalendarRegDate())
 				.calendar_mod_date(employeeCalendar.getCalendarModDate()).build();
