2ffd118da96203dd69f07a1ece00dbda15394646
862yuna <862yuna@gmail.com>
2025-04-15 14:21:29 +0900

    [cya] DynamicReport API 적용중
    	- report.controller 패키지 생성, ReportController.java생성
    	- report.service 패키지 생성, ReportService.java 생성

--- eroom/src/main/java/com/eroom/report/controller/ReportController.java
+++ eroom/src/main/java/com/eroom/report/controller/ReportController.java
@@ -26,18 +26,18 @@ public class ReportController {
 	}
 	
 	// GET 요청으로 sample pdf 리포트를 다운로드할 수 있게 하는 API
-//	@GetMapping("/sample/pdf")
-//	public ResponseEntity<byte[]> downloadSamplePdf() throws Exception{
-//		// Service 에서 PDF 리포트 데이터를 받아옴
-//		ByteArrayOutputStream baos = reportService.generateSimplePdfReport();
-//		
-//		// HTTP 응답 헤더 설정
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.setContentType(MediaType.APPLICATION_PDF); // Content-Type : application/pdf
-//		headers.setContentDisposition(ContentDisposition.attachment().filename("sample_report.pdf").build()); // 다운로드용
-//		
-//		// 실제 PDF 바이트 배열, 헤더, 상태코드를 함꼐 응답으로 보냄
-//		return new ResponseEntity<>(baos.toByteArray(), headers, HttpStatus.OK);
-//	}
+	@GetMapping("/sample/pdf")
+	public ResponseEntity<byte[]> downloadSamplePdf() throws Exception{
+		// Service 에서 PDF 리포트 데이터를 받아옴
+		ByteArrayOutputStream baos = reportService.generateSimplePdfReport();
+		
+		// HTTP 응답 헤더 설정
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_PDF); // Content-Type : application/pdf
+		headers.setContentDisposition(ContentDisposition.attachment().filename("sample_report.pdf").build()); // 다운로드용
+		
+		// 실제 PDF 바이트 배열, 헤더, 상태코드를 함꼐 응답으로 보냄
+		return new ResponseEntity<>(baos.toByteArray(), headers, HttpStatus.OK);
+	}
 	
 }
--- eroom/src/main/java/com/eroom/report/service/ReportService.java
+++ eroom/src/main/java/com/eroom/report/service/ReportService.java
@@ -1,50 +1,62 @@
 package com.eroom.report.service;
 
+import static net.sf.dynamicreports.report.builder.DynamicReports.report;
+import static net.sf.dynamicreports.report.builder.DynamicReports.stl;
+import static net.sf.dynamicreports.report.builder.DynamicReports.type;
+
 import java.io.ByteArrayOutputStream;
 
 import org.springframework.stereotype.Service;
 
-import static net.sf.dynamicreports.report.builder.DynamicReports.*;
-
 import net.sf.dynamicreports.report.builder.column.Columns;
 import net.sf.dynamicreports.report.builder.component.Components;
+import net.sf.dynamicreports.report.builder.style.StyleBuilder;
 import net.sf.dynamicreports.report.datasource.DRDataSource;
 import net.sf.dynamicreports.report.exception.DRException;
 
 @Service
 public class ReportService {
 	
-//	public ByteArrayOutputStream generateSimplePdfReport() throws DRException{
-//		// PDF 데이터를 저장할 출력 스트림 생성
-//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//		
-//		// 리포트에 넣을 샘플 데이터를 정의함
-//		// DRDataSource는 컬럼명 정의 -> add()로 행을 추가한다.
-//		DRDataSource dataSource = new DRDataSource("name","position");
-//		dataSource.add("전홍식","하면해");	
-//		dataSource.add("이예준","응애");	
-//		dataSource.add("강성관","닫은 식당");	
-//		dataSource.add("조윤아","으앙");	
-//		dataSource.add("권용규","국쏘?");	
-//		dataSource.add("조은성","연식당");	
-//		
-//		// 리포트를 생성하는 main 코드 블록
-//		report()
-//			// 출력할 컬럼들을 정의 : 컬럼명, 필드명, 데이터 타입
-//			.columns(
-//				Columns.column("이름","name",type.stringType()),
-//				Columns.column("역할","position",type.stringType())
-//				
-//				)
-//			.title(Components.text("샘플 리포트").setStyle(koreanFont.bold().setFontSize(14)))
-//			.pageFooter(Components.pageXofY())
-//			.setDataSource(dataSource)
-//			.toPdf(baos);
-//		
-//		return baos;
-//		
-//		
-//		
-//	}
+	StyleBuilder koreanFont = stl.style()
+			.setFont(stl.font()
+					.setFontName("Malgun Gothic")
+					.setPdfFontName("fonts/malgun.ttf")
+					.setPdfEncoding("Identity-H")
+					.setPdfEmbedded(true)
+					);
+	
+	
+	public ByteArrayOutputStream generateSimplePdfReport() throws DRException{
+		// PDF 데이터를 저장할 출력 스트림 생성
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		// 리포트에 넣을 샘플 데이터를 정의함
+		// DRDataSource는 컬럼명 정의 -> add()로 행을 추가한다.
+		DRDataSource dataSource = new DRDataSource("name","position");
+		dataSource.add("전홍식","하면해");	
+		dataSource.add("이예준","응애");	
+		dataSource.add("강성관","닫은 식당");	
+		dataSource.add("조윤아","으앙");	
+		dataSource.add("권용규","국쏘?");	
+		dataSource.add("조은성","연식당");	
+		
+		// 리포트를 생성하는 main 코드 블록
+		report()
+			// 출력할 컬럼들을 정의 : 컬럼명, 필드명, 데이터 타입
+			.columns(
+				Columns.column("이름","name",type.stringType()),
+				Columns.column("역할","position",type.stringType())
+				
+				)
+			.title(Components.text("샘플 리포트").setStyle(koreanFont.bold().setFontSize(14)))
+			.pageFooter(Components.pageXofY())
+			.setDataSource(dataSource)
+			.toPdf(baos);
+		
+		return baos;
+		
+		
+		
+	}
 
 }
