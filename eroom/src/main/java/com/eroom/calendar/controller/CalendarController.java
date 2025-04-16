bdf00d9829c402d1dbdf426ba5db1c9fbc6fa786
KYG17 <sjkyg17@gmail.com>
2025-04-16 17:43:26 +0900

    [KYK] 개인 캘린더 일부 수정
    	-등록 시 시간 설정: 시 종료시간이 시작시간보다 과거로 설정하지 못하게
    	-수정할때도 똑같이 할 수 있도록 해야함
    
    	회사 캘린더 진행중
    	-목록 조회
    	-상세 조회
    	-일정 등록
    	-일정 수정 까지 완료

--- eroom/src/main/java/com/eroom/calendar/controller/CalendarController.java
+++ eroom/src/main/java/com/eroom/calendar/controller/CalendarController.java
@@ -13,8 +13,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.eroom.calendar.dto.CompanyCalendarDto;
 import com.eroom.calendar.dto.EmployeeCalendarDto;
+import com.eroom.calendar.entity.CompanyCalendar;
 import com.eroom.calendar.entity.EmployeeCalendar;
+import com.eroom.calendar.service.CompanyCalendarService;
 import com.eroom.calendar.service.EmployeeCalendarService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 public class CalendarController {
 	
 	private final EmployeeCalendarService service;
+	private final CompanyCalendarService companyService;
 
 	@GetMapping("/calendar")
 	public String calendarView() {
@@ -72,35 +76,56 @@ public class CalendarController {
 		return resultMap;
 	}
 	
+	//회사 캘린더 등록
+	@PostMapping("/companycalendar/add")
+	@ResponseBody
+	public Map<String,String> addCompanyCalendarApi(CompanyCalendarDto param){
+		Map<String,String> resultMap = new HashMap<String,String>();
+		resultMap.put("res_code", "500");
+		resultMap.put("res_msg", "일정 등록을 실패하였습니다");
+		System.out.println(param);
+		
+		CompanyCalendarDto cdto = companyService.addCompanyCalendar(param);
+		if(cdto != null) {
+			resultMap.put("res_code", "200");
+			resultMap.put("res_msg", "일정을 등록하였습니다!");
+		}
+		
+		return resultMap;
+	}
+	
+	
+	
+	
 	//해당 유저의 일정 목록을 조회
 	@GetMapping("/employeecalendar/list/{employeeNo}")
 	@ResponseBody
 	public List<Map<String, Object>> getCalendarList(@PathVariable("employeeNo") Long employeeNo) {
 	    List<Map<String, Object>> result = new ArrayList<>();
 	    List<EmployeeCalendarDto> calendarList = service.getCalendarList(employeeNo);
+	    
 	    for (EmployeeCalendarDto dto : calendarList) {
-	   
 	        result.add(dto.toFullCalendarEvent());
 	    }
 	    
 	    return result;
 	}
 	
-	/*@PostMapping("/calendar/update/{calendarNo}")
-	public Map<String,String>updateCalendar(@PathVariable Long calendarNo, Model model){
-		Map<String,String> resultMap = new HashMap<String,String>();
-		resultMap.put("res_code", "500");
-		resultMap.put("res_msg", "일정 수정을 실패하였습니다");
-		
-		System.out.println(calendarNo);*/
-		
-//		EmployeeCalendarDto edto = service.updateCalendar(calendarNo);
-//		if(edto != null) {
-//			resultMap.put("res_code", "200");
-//			resultMap.put("res_msg", "수정을 성공하였습니다!");
-//		}
-//		return resultMap;
+	//회사 일정의 목록을 조회 -> 구분자로 판별
+	@GetMapping("/companycalendar/list/{separator}")
+	@ResponseBody
+	public List<Map<String, Object>> getCompanyList(@PathVariable("separator") String separator) {
+	    List<Map<String, Object>> result = new ArrayList<>();
+	    List<CompanyCalendarDto> companyList = companyService.getCompanyList(separator);
+	    
+	    for (CompanyCalendarDto dto : companyList) {
+	        result.add(dto.toFullCalendarEvent());
+	    }
+	    
+	    return result;
+	}
 	
+	//해당 직원 일정 단일 조회 (for 수정 모달)
     @GetMapping("/employeecalendar/detail/{calendarNo}")
     @ResponseBody
     public ResponseEntity<EmployeeCalendarDto> selectCalendarOne(@PathVariable("calendarNo") Long calendarNo) {
@@ -115,7 +140,23 @@ public class CalendarController {
         }
     }
     
-    //일정 수정 모달에서 데이터 받아와서 저장하는 메소드
+    //회사 일정 단일 조회 (for 수정 모달)
+    @GetMapping("/companycalendar/detail/{calendarNo}")
+    @ResponseBody
+    public ResponseEntity<CompanyCalendarDto> selectCompanyOne(@PathVariable("calendarNo") Long calendarNo){
+        try {
+        	CompanyCalendarDto calendar = companyService.findByCompanyNo(calendarNo);
+            if (calendar == null) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(calendar);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+    
+    
+    //개인 일정 수정 모달에서 데이터 받아와서 저장하는 메소드
     @PostMapping("/employeecalendar/update/{calendarNo}")
     @ResponseBody
     public Map<String,String> updateCalendar(EmployeeCalendarDto param,@PathVariable("calendarNo") Long calendarNo){
@@ -137,7 +178,29 @@ public class CalendarController {
 		}
 		return resultMap;
     }
+    
+    //회사 일정 수정 모달에서 데이터 받아와서 저장하는 메소드
+    @PostMapping("/companycalendar/update/{calendarNo}")
+    @ResponseBody
+    public Map<String,String> updateCompanyCalendar(CompanyCalendarDto param, @PathVariable("calendarNo") Long calendarNo){
+    	Map<String,String> resultMap = new HashMap<String,String>();
+    	resultMap.put("res_code", "500");
+    	resultMap.put("res_msg", "일정 수정을 실패하였습니다");
+    	
+    	param.setCalendar_no(calendarNo);
+    	
+    	CompanyCalendar update = companyService.updateCompanyCalendar(param);
+    	
+    	if(update != null) {
+			resultMap.put("res_code", "200");
+			resultMap.put("res_msg", "수정을 성공하였습니다!");
+		}
+    	
+    	
+    	return resultMap;
+    }
 	
+    //일정 삭제 --> calendar 테이블에 사용여부를 Y -> N으로 변경
     @PostMapping("/employeecalendar/delete/{calendarNo}")
     @ResponseBody
     public Map<String,String> deleteCalendar(@PathVariable("calendarNo") Long id){
