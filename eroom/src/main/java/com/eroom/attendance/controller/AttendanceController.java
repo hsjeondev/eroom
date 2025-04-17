46b626c64f4fc4e1c059d77b0ba92198280968f9
862yuna <862yuna@gmail.com>
2025-04-17 19:41:37 +0900

    [cya] 근태 페이지 작업중

--- eroom/src/main/java/com/eroom/attendance/controller/AttendanceController.java
+++ eroom/src/main/java/com/eroom/attendance/controller/AttendanceController.java
@@ -1,22 +1,20 @@
 package com.eroom.attendance.controller;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.eroom.attendance.dto.AttendanceDto;
 import com.eroom.attendance.entity.Attendance;
 import com.eroom.attendance.repository.AttendanceRepository;
 import com.eroom.attendance.service.AttendanceService;
+import com.eroom.directory.entity.Directory;
+import com.eroom.directory.service.EmployeeDirectoryService;
+import com.eroom.employee.entity.Employee;
+import com.eroom.employee.entity.Structure;
 import com.eroom.security.EmployeeDetails;
 
 import lombok.RequiredArgsConstructor;
@@ -28,12 +26,27 @@ public class AttendanceController {
 	
 	private final AttendanceService attendanceService;
 	private final AttendanceRepository attendanceRepository;
+	private final EmployeeDirectoryService employeeDirectoryService;
 	
 	// 근태 페이지 목록
 	@GetMapping("/list")
-	public String selectAttendanceList(Model model) {
-//		 List<Attendance> resultList = attendanceService.selectAttendanceList();
-//		model.addAttribute("attendanceList",resultList);
+	public String selectAttendanceList(Model model, Authentication authentication) {
+		
+		EmployeeDetails employeeDetail = (EmployeeDetails) authentication.getPrincipal();
+		Employee employee = employeeDetail.getEmployee();
+		Long employeeNo = employee.getEmployeeNo();
+		model.addAttribute("employee", employee);
+		
+		// 부서 정보 조회
+		Structure structure = employeeDirectoryService.selectStructureCodeNameByParentCodeEqualsSeparatorCode(employee.getStructure().getParentCode());
+        String departmentName = structure != null ? structure.getCodeName() : "-";
+        model.addAttribute("departmentName", departmentName);
+        
+        // 주소록 정보 조회
+        // Directory directory = employeeDirectoryService.selectDirectoryByEmployeeNo(employeeNo);
+		
+		List<Attendance> resultList = attendanceService.selectAttendanceList();
+		model.addAttribute("attendanceList",resultList);
 		return "attendance/list";
 	}
 	
