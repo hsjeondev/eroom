1f75f523a6ffc36eec19b068b373bce395a4d3ba
KYG17 <sjkyg17@gmail.com>
2025-04-22 14:10:37 +0900

    [KYK] 차량 예약하기 목록조회 차량 목록진행중

--- eroom/src/main/java/com/eroom/reservation/controller/ReservationController.java
+++ eroom/src/main/java/com/eroom/reservation/controller/ReservationController.java
@@ -1,17 +1,37 @@
 package com.eroom.reservation.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.eroom.facility.entity.Facility;
+import com.eroom.reservation.service.VehicleService;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class ReservationController {
+
+   
+	
+	private final VehicleService vehicleService;
+
+  
+	
 	@GetMapping("/reservation/sleep")
 	public String sleepReservationView() {
 		return "reservation/sleeprev";
 	}
 	
 	@GetMapping("/reservation/vehicle")
-	public String vehicleReservationView() {
+	public String vehicleReservationView(Model model) {
+		List<Facility>result = vehicleService.selectVehicleAll();
+		//목록이 정상적으로 출력
+		//System.out.println(result);
+		model.addAttribute("list",result);		
 		return "reservation/vehiclerev";
 	}
 	
