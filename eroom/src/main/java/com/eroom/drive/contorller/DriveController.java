a91a1224693f4f5e77d82a83d1341961e582908a
devyej8753 <devyej8753@gmail.com>
2025-04-24 15:05:01 +0900

    [lyj] 2025-04-24 개인 드라이브 작업중
    	- drive DB 컬럼 추가
    	- application.properties 에 파일업로드 위치 저장(C:/dev/errom/upload/)
    	- EroomApplication 파일 업로드 메소드 추가
    	- personal.html 에서 부트스트랩 멀티파일 가져와봄
    	- 파일 업로드 작업중

--- eroom/src/main/java/com/eroom/drive/contorller/DriveController.java
+++ eroom/src/main/java/com/eroom/drive/contorller/DriveController.java
@@ -1,8 +1,16 @@
 package com.eroom.drive.contorller;
 
+import java.util.Map;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.eroom.drive.dto.DriverDto;
+import com.eroom.security.EmployeeDetails;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,9 +34,15 @@ public class DriveController {
 	public String selectDriveTeam() {
 		return "drive/team";
 	}
-	// 개인
+	// 개인 드라이브
 	@GetMapping("/personal")
-	public String selectDrivePersonal() {
+	public String selectDrivePersonal(Model model, @AuthenticationPrincipal EmployeeDetails user) {
 		return "drive/personal";
 	}
+	// 드라이브 업로드
+//	@GetMapping("/create")
+//	@ResponseBody
+//	public Map<String,String> createDrive(DriverDto driverDto @RequestParam String){
+//		
+//	}
 }
---
+++ eroom/src/main/java/com/eroom/drive/service/DriverService.java
@@ -0,0 +1,5 @@
+package com.eroom.drive.service;
+
+public class DriverService {
+
+}
