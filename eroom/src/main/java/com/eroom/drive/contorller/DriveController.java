b6d76114c3f6cb78c5805b1fa62f4aa6d75b7f69
devyej8753 <devyej8753@gmail.com>
2025-04-24 19:37:37 +0900

    [lyj] 2025-04-24 개인 드라이브 작업중
    	- 파일 수정 완성
    	- 파일 교체, 파일 설명만 수정 할 수 있음

--- eroom/src/main/java/com/eroom/drive/contorller/DriveController.java
+++ eroom/src/main/java/com/eroom/drive/contorller/DriveController.java
@@ -8,10 +8,12 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.eroom.drive.dto.DriveDto;
 import com.eroom.drive.service.DriveService;
@@ -68,6 +70,33 @@ public class DriveController {
 		} 
 		return resultMap;
 	}
+	// --------------------------------- 파일 수정 ------------------------------------------
+	@PostMapping("/update/{attachNo}")
+	@ResponseBody
+	public Map<String, String> editDriveFile(@PathVariable("attachNo") Long attachNo,
+	                                         @RequestParam(value = "driveFile", required = false) MultipartFile file,
+	                                         @RequestParam("driveDescription") String description) {
+	    Map<String, String> result = new HashMap<>();
+	    result.put("res_code", "500");
+	    result.put("res_msg", "수정 실패");
+
+	    boolean success = driveService.updateDriveFile(attachNo, file, description);
+	    if (success) {
+	        result.put("res_code", "200");
+	        result.put("res_msg", "수정 완료");
+	    }
+
+	    return result;
+	}
+
+
+	
+	
+	// ---------------------------------- 파일 다운로드 ------------------------------------------
+	
+	
+	
+	// -------------------------------------------- 파일 삭제 ------------------------------------------
 	
 	
 }
