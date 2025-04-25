4806f0de38778e17caddc8483ea0cbb50fb45b29
hsjeondev <hsjeondev@gmail.com>
2025-04-25 17:37:49 +0900

    [jhs] 프로젝트 디테일 todo 칸반보드 리스트 추가 작업

--- eroom/src/main/java/com/eroom/project/repository/ProjectTodoListRepository.java
+++ eroom/src/main/java/com/eroom/project/repository/ProjectTodoListRepository.java
@@ -1,5 +1,7 @@
 package com.eroom.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -7,4 +9,5 @@ import com.eroom.project.entity.ProjectTodoList;
 
 public interface ProjectTodoListRepository extends JpaRepository<ProjectTodoList, Long>, JpaSpecificationExecutor<ProjectTodoList> {
 
+	List<ProjectTodoList> findByProjectNoOrderByListSequenceAsc(Long projectNo);
 }
