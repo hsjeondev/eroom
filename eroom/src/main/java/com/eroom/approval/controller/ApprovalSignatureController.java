6d58758d004acd062f221599610227a467048b8f
devkskg <devkskg@gmail.com>
2025-04-28 22:38:57 +0900

    [ksk] 시그니처패드 기능 진행중2_20250428
    	- 시그니처패드 Entity, Dto 수정 완료
    	- 시그니처패드 불러오기 성공
    	- layout에 SignaturePad 라이브러리 코드 추가

---
+++ eroom/src/main/java/com/eroom/approval/controller/ApprovalSignatureController.java
@@ -0,0 +1,15 @@
+package com.eroom.approval.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ApprovalSignatureController {
+	
+//	private final
+	
+	
+
+}
