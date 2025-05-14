3d2d525f3c2dc0f4cfdbf59178ea70247e246bb9
hsjeondev <hsjeondev@gmail.com>
2025-05-14 16:52:04 +0900

    Update deploy-dev.sh

--- springboot-docker/deploy-dev.sh
+++ springboot-docker/deploy-dev.sh
@@ -15,22 +15,22 @@ else
 fi
 
 # 2. 설정 파일 복사
-echo "[INFO] application.yml, application-secret.properties 복사 중...."
+echo "[INFO] application.yml, application-secret.properties 복사 중..."
 mkdir -p ./src/main/resources/
 cp ./springboot-docker/secrets/application.yml ./src/main/resources/
 cp ./springboot-docker/secrets/application-secret.properties ./src/main/resources/
 
 # 3. 기존 컨테이너 종료
 echo "[INFO] 기존 컨테이너 종료 중..."
-docker-compose --env-file "$ENV_PATH" down
+docker compose --env-file "$ENV_PATH" down
 
 # 4. 이미지 빌드
 echo "[INFO] 이미지 재빌드 중..."
-docker-compose --env-file "$ENV_PATH" build --no-cache
+docker compose --env-file "$ENV_PATH" build --no-cache
 
 # 5. 컨테이너 실행
 echo "[INFO] 컨테이너 재기동 중..."
-docker-compose --env-file "$ENV_PATH" up -d
+docker compose --env-file "$ENV_PATH" up -d
 
 # 6. 기존 cloudflared 컨테이너 제거
 if docker ps -a --format '{{.Names}}' | grep -q '^cloudflared$'; then
@@ -43,4 +43,4 @@ echo "[INFO] cloudflared tunnel 실행 중..."
 docker run -d --name cloudflared cloudflare/cloudflared:latest \
   tunnel --no-autoupdate run --token "$CLOUDFLARED_TOKEN"
 
-echo "[INFO] 배포 완료 ✅ : $(date)"
+echo "[INFO] 배포 완료 ? : $(date)"
